dcfb5c5e802982a6f0c6d545ba5c7c6d5c0b27c3
vypiemzalyubov <dima.rza90@mail.ru>
2023-08-05 10:45:21 +0300

    Update sql alchemy task

--- SQL Academy/sql_academy.sql
+++ SQL Academy/sql_academy.sql
@@ -41,6 +41,11 @@ WHERE town_to = 'Moscow';
 
 -- 8. В какие города можно улететь из Парижа (Paris) и сколько времени это займёт?
 
+SELECT town_to,
+	TIMEDIFF(time_in, time_out) AS flight_time
+FROM Trip
+WHERE town_from = 'Paris';
+
 -- 9. Какие компании организуют перелеты из Владивостока (Vladivostok)?
 
 SELECT name
