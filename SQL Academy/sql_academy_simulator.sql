0d9e72d0c3e86330906328e9a046ce68f8eedd09
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2023-08-10 09:00:14 +0300

    Update sql academy task

--- SQL Academy/sql_academy_simulator.sql
+++ SQL Academy/sql_academy_simulator.sql
@@ -308,3 +308,20 @@ WHERE last_name = 'Romashkin'
 SELECT start_pair
 FROM Timepair
 WHERE id = 4;
+
+-- 42. Сколько времени обучающийся будет находиться в школе, учась со 2-го по 4-ый уч. предмет?
+
+SELECT TIMEDIFF(
+		(
+			SELECT end_pair
+			FROM Timepair
+			WHERE id = 4
+		),
+		(
+			SELECT start_pair
+			FROM Timepair
+			WHERE id = 2
+		)
+	) AS time
+FROM Timepair
+LIMIT 1;
