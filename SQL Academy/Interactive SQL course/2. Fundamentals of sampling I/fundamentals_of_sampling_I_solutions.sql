17233225dfae0bdeb2e644e00fe11926b1277469
vypiemzalyubov <dima.rza90@mail.ru>
2023-08-19 09:32:38 +0300

    Update solutions

--- SQL Academy/Interactive SQL course/2. Fundamentals of sampling I/fundamentals_of_sampling_I_solutions.sql
+++ SQL Academy/Interactive SQL course/2. Fundamentals of sampling I/fundamentals_of_sampling_I_solutions.sql
@@ -102,3 +102,133 @@ WHERE
   has_kitchen = 1 
   AND has_internet = 1;
 
+-- 14. Выведите имена first_name и фамилии last_name студентов из таблицы Student, у кого отсутствует отчество middle_name.
+
+SELECT 
+  first_name, 
+  last_name 
+FROM
+  Student 
+WHERE 
+  middle_name IS NULL;
+
+-- 15. Выведите резервации комнат (поля room_id, start_date, end_date) из таблицы Reservations, у которых итоговая стоимость аренды (поле total) находится в промежутке от 500 до 1200 включительно.
+
+SELECT 
+  room_id, 
+  start_date, 
+  end_date 
+FROM 
+  Reservations 
+WHERE 
+  total BETWEEN 500 
+  AND 1200;
+
+-- 16. Выведите информацию о студентах из таблицы Student, у которых год рождения соответствует одном из перечисленных: 2000, 2002 и 2004.
+
+SELECT 
+  * 
+FROM
+  Student 
+WHERE 
+  year(birthday) IN (2000, 2002, 2004);
+
+-- 17. Найдите всех членов семьи с фамилией "Quincey" и выведите поле member_name.
+
+SELECT 
+  member_name 
+FROM 
+  FamilyMembers 
+WHERE 
+  member_name like '%Quincey';
+
+-- 18. Для каждого отдельного платежа выведите идентификатор товара и сумму, потраченную на него, в отсортированном по убыванию этой суммы виде. Список платежей находится в таблице Payments.
+--     Для вывода суммы используйте псевдоним sum.
+
+SELECT 
+  good, 
+  amount * unit_price AS sum 
+FROM
+  Payments 
+ORDER BY 
+  sum DESC;
+
+-- 19. Выведите список членов семьи с фамилией Quincey, в отсортированном по возрастанию столбцам status и member_name виде.
+
+SELECT 
+  * 
+FROM
+  FamilyMembers 
+WHERE 
+  member_name LIKE '%Quincey' 
+ORDER BY 
+  status, 
+  member_name;
+
+-- 20. Подсчитайте количество учеников в каждом классе, а также отсортируйте их по убыванию количества учеников. Принадлежность ученика к конкретному классу вы можете получить
+--     из таблицы Student_in_class. В качестве результата необходимо вывести идентификатор класса (поле class) и количество учеников в этом классе.
+--     Для вывода количества учеников используйте псевдоним count.
+
+SELECT 
+  class, 
+  COUNT(*) AS count 
+FROM 
+  Student_in_class 
+GROUP BY 
+  class 
+ORDER BY 
+  count DESC;
+
+-- 21. Для каждого из существующих статусов (поле status) найдите самого старого человека (используйте поле birthday). Выведите статус и дату рождения.
+--     Для вывода даты рождения используйте псевдоним birthday.
+
+SELECT 
+  status, 
+  MIN(birthday) AS birthday 
+FROM 
+  FamilyMembers 
+GROUP BY 
+  status;
+
+-- 22. Получите среднее время полётов, совершённых на каждой из моделей самолёта. Выведите поле plane и среднее время полёта в секундах. Для вывода времени используйте псевдоним time.
+--     Используйте функцию TIMESTAMPDIFF(second, time_out, time_in), чтобы получить разницу во времени в секундах между двумя датами.
+
+SELECT 
+  plane, 
+  AVG(
+    TIMESTAMPDIFF(second, time_out, time_in)
+  ) AS time 
+FROM 
+  Trip 
+GROUP BY 
+  plane;
+
+-- 23. Выведите идентификатор комнаты (поле room_id), среднюю стоимость за один день аренды (поле price, для вывода используйте псевдоним avg_price), 
+--     а также количество резерваций этой комнаты (используйте псевдоним count). Полученный результат отсортируйте в порядке убывания сначала по количеству резерваций, а потом по средней стоимости.
+
+SELECT 
+  room_id, 
+  AVG(price) AS avg_price, 
+  COUNT(*) AS count 
+Friel 
+  Reservations 
+GROUP BY 
+  room_id 
+ORDER BY 
+  count DESC, 
+  avg_price DESC;
+
+-- 24. Выведите типы комнат (поле home_type) и разницу между самым дорогим и самым дешевым представителем данного типа. 
+--     В итоговую выборку включите только те типы жилья, количество которых в таблице Rooms больше или равно 2. Для вывода разницы стоимости используйте псевдоним difference.
+
+SELECT 
+  home_type, 
+  (
+    MAX(price) - MIN(price)
+  ) AS difference 
+FROM 
+  Rooms 
+GROUP BY 
+  home_type 
+HAVING 
+  count(*) > 2;
